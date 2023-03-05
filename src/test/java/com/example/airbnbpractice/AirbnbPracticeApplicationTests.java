39650523c8250985b1c9d74c3ffdb5112d3c8c30
kikidy12 <30651543+kikidy12@users.noreply.github.com>
2023-03-05 13:29:57 +0900

    Update AirbnbPracticeApplicationTests.java

--- src/test/java/com/example/airbnbpractice/AirbnbPracticeApplicationTests.java
+++ src/test/java/com/example/airbnbpractice/AirbnbPracticeApplicationTests.java
@@ -5,6 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AirbnbPracticeApplicationTests {
+	
+	
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 
 	@Test
 	void contextLoads() {
